7935226d55d12cc80bca9f6a37abcfba0784dfa5
Christopher Grote <cmgrote@users.noreply.github.com>
2022-08-10 01:22:55 +0100

    Adds connections
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

---
+++ src/main/java/com/atlan/model/Connection.java
@@ -0,0 +1,136 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+package com.atlan.model;
+
+import com.atlan.exception.InvalidRequestException;
+import com.atlan.model.enums.AtlanConnectionCategory;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Instance of a connection in Atlan, with its detailed information.
+ */
+@Getter
+@Setter
+@SuperBuilder(toBuilder = true)
+@EqualsAndHashCode(callSuper = true)
+public class Connection extends Asset {
+    private static final long serialVersionUID = 2L;
+
+    public static final String TYPE_NAME = "Connection";
+
+    /** Fixed typeName for column processes. */
+    @Getter(onMethod_ = {@Override})
+    @Setter(onMethod_ = {@Override})
+    @Builder.Default
+    String typeName = TYPE_NAME;
+
+    /** Type of connection. */
+    @Attribute
+    AtlanConnectionCategory category;
+
+    /** Unused attributes */
+    transient String subCategory;
+    transient Map<String, String> queryPreviewConfig;
+    transient String queryConfig;
+
+    /** Host name of the connection's source. */
+    @Attribute
+    String host;
+
+    /** Port number to the connection's source. */
+    @Attribute
+    Integer port;
+
+    /** When true, allow the source to be queried. */
+    @Attribute
+    Boolean allowQuery;
+
+    /** When true, allow data previews of the source. */
+    @Attribute
+    Boolean allowQueryPreview;
+
+    /** TBC */
+    @Attribute
+    String credentialStrategy;
+
+    /** Maximum number of rows that can be returned for the source. */
+    @Attribute
+    Long rowLimit;
+
+    /** TBC */
+    @Attribute
+    String defaultCredentialGuid;
+
+    /** TBC */
+    @Attribute
+    String connectorIcon;
+
+    /** TBC */
+    @Attribute
+    String connectorImage;
+
+    /** TBC */
+    @Attribute
+    String sourceLogo;
+
+    /**
+     * Builds the minimal object necessary to create a connection.
+     * To continue adding to the object, call {@link #toBuilder()} on the result and continue calling
+     * additional methods to add metadata followed by {@link ConnectionBuilder#build()}.
+     * Note: at least one of {@code #adminRoles}, {@code #adminGroups}, or {@code #adminUsers} must be
+     * provided or an InvalidRequestException will be thrown.
+     *
+     * @param name of the connection
+     * @param category type of connection
+     * @param connectorName name of the connection's connector (this determines what logo appears for the assets)
+     * @param adminRoles the GUIDs of the roles that can administer this connection
+     * @param adminGroups the names of the groups that can administer this connection
+     * @param adminUsers the names of the users that can administer this connection
+     * @return the minimal object necessary to create the connection
+     * @throws InvalidRequestException if no admin has been defined for the connection
+     */
+    public static Connection toCreate(String name,
+                                      AtlanConnectionCategory category,
+                                      String connectorName,
+                                      List<String> adminRoles,
+                                      List<String> adminGroups,
+                                      List<String> adminUsers) throws InvalidRequestException {
+        boolean adminFound = false;
+        ConnectionBuilder<?,?> builder = Connection.builder()
+            .name(name)
+            .qualifiedName(generateQualifiedName(connectorName))
+            .category(category)
+            .connectorName(connectorName);
+        if (adminRoles != null && !adminRoles.isEmpty()) {
+            adminFound = true;
+            builder = builder.adminRoles(adminRoles);
+        }
+        if (adminGroups != null && !adminGroups.isEmpty()) {
+            adminFound = true;
+            builder = builder.adminGroups(adminGroups);
+        }
+        if (adminUsers != null && !adminUsers.isEmpty()) {
+            adminFound = true;
+            builder = builder.adminUsers(adminUsers);
+        }
+        if (adminFound) {
+            return builder.build();
+        } else {
+            throw new InvalidRequestException("No admin provided for the connection, will not attempt to create one.", "adminRoles,adminGroups,adminUsers", "--", "ATLAN-CLIENT-CONNECTION-400-001", 400, null);
+        }
+    }
+
+    /**
+     * Generate a unique connection name.
+     * @param connectorName name of the connection's connector
+     * @return a unique name for the connection
+     */
+    private static String generateQualifiedName(String connectorName) {
+        long now = System.currentTimeMillis() / 1000;
+        return "default/" + connectorName + "/" + now;
+    }
+}
---
+++ src/test/java/com/atlan/model/ConnectionTest.java
@@ -0,0 +1,118 @@
+package com.atlan.model;
+
+import static org.testng.Assert.*;
+
+import com.atlan.model.enums.AtlanAnnouncementType;
+import com.atlan.model.enums.AtlanCertificateStatus;
+import com.atlan.model.enums.AtlanStatus;
+import com.atlan.model.relations.Reference;
+import com.atlan.net.ApiResource;
+import org.testng.annotations.Test;
+
+public class ConnectionTest {
+
+    private static final Connection full = Connection.builder()
+        .guid("guid")
+        .displayText("displayText")
+        .status(AtlanStatus.ACTIVE)
+        .createdBy("createdBy")
+        .updatedBy("updatedBy")
+        .createTime(123456789L)
+        .updateTime(123456789L)
+        .isIncomplete(false)
+        .qualifiedName("qualifiedName")
+        .name("name")
+        .displayName("displayName")
+        .description("description")
+        .userDescription("userDescription")
+        .tenantId("tenantId")
+        .certificateStatus(AtlanCertificateStatus.VERIFIED)
+        .certificateStatusMessage("certificateStatusMessage")
+        .certificateUpdatedBy("certificateUpdatedBy")
+        .certificateUpdatedAt(123456789L)
+        .announcementTitle("announcementTitle")
+        .announcementMessage("announcementMessage")
+        .announcementUpdatedAt(123456789L)
+        .announcementUpdatedBy("announcementUpdatedBy")
+        .announcementType(AtlanAnnouncementType.INFORMATION)
+        .ownerUser("ownerUser")
+        .ownerGroup("ownerGroup")
+        .adminUser("adminUser")
+        .adminGroup("adminGroup")
+        .adminRole("adminRole")
+        .viewerUser("viewerUser")
+        .viewerGroup("viewerGroup")
+        .connectorName("connectorName")
+        .connectionName("connectionName")
+        .connectionQualifiedName("connectionQualifiedName")
+        .__hasLineage(false)
+        .isDiscoverable(true)
+        .isEditable(true)
+        .viewScore(123456.0)
+        .popularityScore(123456.0)
+        .sourceOwners("sourceOwners")
+        .sourceURL("sourceURL")
+        .sourceEmbedURL("sourceEmbedURL")
+        .lastSyncWorkflowName("lastSyncWorkflowName")
+        .lastSyncRunAt(123456789L)
+        .lastSyncRun("lastSyncRun")
+        .sourceCreatedBy("sourceCreatedBy")
+        .sourceCreatedAt(123456789L)
+        .sourceUpdatedAt(123456789L)
+        .sourceUpdatedBy("sourceUpdatedBy")
+        .link(Reference.to("Resource", "linkGuid1"))
+        .link(Reference.to("Resource", "linkGuid2"))
+        .readme(Reference.to("Readme", "readmeGuid"))
+        .meaning(Reference.to(GlossaryTerm.TYPE_NAME, "termGuid1"))
+        .meaning(Reference.to(GlossaryTerm.TYPE_NAME, "termGuid2"))
+        .host("host")
+        .port(1234)
+        .allowQuery(true)
+        .allowQueryPreview(true)
+        .credentialStrategy("credentialStrategy")
+        .rowLimit(123456L)
+        .defaultCredentialGuid("defaultCredentialGuid")
+        .connectorIcon("connectorIcon")
+        .connectorImage("connectorImage")
+        .sourceLogo("sourceLogo")
+        .build();
+
+    private static Connection frodo;
+    private static String serialized;
+
+    @Test(groups = {"serialize"})
+    void serialization() {
+        assertNotNull(full);
+        serialized = full.toJson();
+        assertNotNull(serialized);
+    }
+
+    @Test(
+        groups = {"deserialize"},
+        dependsOnGroups = {"serialize"})
+    void deserialization() {
+        assertNotNull(serialized);
+        frodo = ApiResource.GSON.fromJson(serialized, Connection.class);
+        assertNotNull(frodo);
+    }
+
+    @Test(
+        groups = {"equivalency"},
+        dependsOnGroups = {"serialize", "deserialize"})
+    void serializedEquivalency() {
+        assertNotNull(serialized);
+        assertNotNull(frodo);
+        String backAgain = frodo.toJson();
+        assertEquals(serialized, backAgain, "Serialization is equivalent after serde loop.");
+    }
+
+    // TODO: Determine why the deserialized form would differ
+    @Test(
+        groups = {"equivalency"},
+        dependsOnGroups = {"serialize", "deserialize"})
+    void deserializedEquivalency() {
+        assertNotNull(full);
+        assertNotNull(frodo);
+        assertEquals(full, frodo, "Deserialization is equivalent after serde loop.");
+    }
+}
