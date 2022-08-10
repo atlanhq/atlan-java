0ede96cc6bcc2b328700148b69a082bfd149cbab
Christopher Grote <cmgrote@users.noreply.github.com>
2022-08-10 01:25:18 +0100

    Enumerates connection categories
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

---
+++ src/main/java/com/atlan/model/enums/AtlanConnectionCategory.java
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+package com.atlan.model.enums;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+
+public enum AtlanConnectionCategory implements AtlanEnum {
+    @SerializedName("warehouse")
+    WAREHOUSE("warehouse"),
+
+    @SerializedName("bi")
+    BI("bi"),
+
+    @SerializedName("ObjectStore")
+    OBJECT_STORE("ObjectStore");
+
+    @Getter(onMethod_ = {@Override})
+    private final String value;
+
+    AtlanConnectionCategory(String value) {
+        this.value = value;
+    }
+
+    public static AtlanConnectionCategory fromValue(String value) {
+        for (AtlanConnectionCategory b : AtlanConnectionCategory.values()) {
+            if (b.value.equals(value)) {
+                return b;
+            }
+        }
+        return null;
+    }
+}
