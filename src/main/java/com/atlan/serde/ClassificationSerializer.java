ba2ffaafd2da436e7b4f1fb29b00e7ea26e0216d
Ernest Hill <ernest@atlan.com>
2022-10-06 16:52:00 +0300

    Add transient tag to eliminote not serializable error

--- src/main/java/com/atlan/serde/ClassificationSerializer.java
+++ src/main/java/com/atlan/serde/ClassificationSerializer.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 public class ClassificationSerializer extends StdSerializer<Classification> {
     private static final long serialVersionUID = 2L;
 
-    private final JsonSerializer<Classification> defaultSerializer;
+    private transient final JsonSerializer<Classification> defaultSerializer;
 
     public ClassificationSerializer() {
         this(null);
