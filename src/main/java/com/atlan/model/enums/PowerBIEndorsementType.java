04e234c1268b1e371a963c775a51944e8796fcb5
Christopher Grote <cmgrote@users.noreply.github.com>
2022-10-06 18:09:48 +0100

    Adds enum for Power BI
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

---
+++ src/main/java/com/atlan/model/enums/PowerBIEndorsementType.java
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright 2022 Atlan Pte. Ltd. */
+package com.atlan.model.enums;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+public enum PowerBIEndorsementType implements AtlanEnum {
+    PROMOTED("Promoted"),
+    CERTIFIED("Certified");
+
+    @JsonValue
+    @Getter(onMethod_ = {@Override})
+    private final String value;
+
+    PowerBIEndorsementType(String value) {
+        this.value = value;
+    }
+
+    public static PowerBIEndorsementType fromValue(String value) {
+        for (PowerBIEndorsementType b : PowerBIEndorsementType.values()) {
+            if (b.value.equals(value)) {
+                return b;
+            }
+        }
+        return null;
+    }
+}
