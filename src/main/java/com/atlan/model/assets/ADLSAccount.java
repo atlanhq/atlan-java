2a2aeea352cbf06f24f5975ebe5cd7d6713fd75f
Christopher Grote <cmgrote@users.noreply.github.com>
2023-02-08 16:47:36 +0000

    Model updates
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- src/main/java/com/atlan/model/assets/ADLSAccount.java
+++ src/main/java/com/atlan/model/assets/ADLSAccount.java
@@ -42,10 +42,6 @@ public class ADLSAccount extends ADLS {
 
     /** TBC */
     @Attribute
-    String adlsAccountName;
-
-    /** TBC */
-    @Attribute
     String adlsAccountResourceGroup;
 
     /** TBC */
