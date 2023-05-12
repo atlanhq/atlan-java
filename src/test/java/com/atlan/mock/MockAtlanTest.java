a358a1f6abcdd79e90247ee34707f5569e661a54
Christopher Grote <cmgrote@users.noreply.github.com>
2023-05-12 16:53:56 +0100

    Removes unused AbstractProcess assets and adds mock API stubs for cache-based unit tests
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

---
+++ src/test/java/com/atlan/mock/MockAtlanTest.java
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright 2023 Atlan Pte. Ltd. */
+package com.atlan.mock;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import com.atlan.Atlan;
+import com.atlan.model.enums.AtlanTypeCategory;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class MockAtlanTest {
+
+    private static WireMockServer server = null;
+
+    @BeforeSuite
+    void startServer() {
+        server = new WireMockServer(options().port(8765));
+        server.start();
+        setStubs();
+        Atlan.setBaseUrl("http://localhost:8765");
+        Atlan.setApiToken("unused");
+    }
+
+    private void setStubs() {
+        server.stubFor(get(urlEqualTo("/api/meta/types/typedefs?type="
+                        + AtlanTypeCategory.CUSTOM_METADATA.getValue().toLowerCase()))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("typedefs-custom-metadata.json")
+                        .withStatus(200)));
+        server.stubFor(get(urlEqualTo("/api/meta/types/typedefs?type="
+                        + AtlanTypeCategory.CLASSIFICATION.getValue().toLowerCase()))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("typedefs-classification.json")
+                        .withStatus(200)));
+        server.stubFor(get(urlEqualTo("/api/meta/types/typedefs?type="
+                        + AtlanTypeCategory.ENUM.getValue().toLowerCase()))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("typedefs-enum.json")
+                        .withStatus(200)));
+    }
+
+    @AfterSuite
+    void stopServer() {
+        server.stop();
+    }
+}
