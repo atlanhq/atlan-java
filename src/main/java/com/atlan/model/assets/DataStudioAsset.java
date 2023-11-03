49480b176a4931859452a606d7febbad19e890ee
Christopher Grote <cmgrote@users.noreply.github.com>
2023-11-03 11:33:08 +0000

    Changes the logic for generating Google Data Studio asset qualifiedNames
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- src/main/java/com/atlan/model/assets/DataStudioAsset.java
+++ src/main/java/com/atlan/model/assets/DataStudioAsset.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 import javax.annotation.processing.Generated;
 import lombok.*;
@@ -435,13 +436,39 @@ public class DataStudioAsset extends Asset
      */
     public static DataStudioAssetBuilder<?, ?> creator(
             String name, String connectionQualifiedName, GoogleDataStudioAssetType assetType) {
+        return DataStudioAsset.creator(
+                name, connectionQualifiedName, assetType, UUID.randomUUID().toString());
+    }
+
+    /**
+     * Builds the minimal object necessary to create a Google Data Studio asset.
+     *
+     * @param name of the asset
+     * @param connectionQualifiedName unique name of the connection through which the asset is accessible
+     * @param assetType type of the asset
+     * @param gdsId unique identifier of the asset within Google Data Studio
+     * @return the minimal object necessary to create the asset, as a builder
+     */
+    public static DataStudioAssetBuilder<?, ?> creator(
+            String name, String connectionQualifiedName, GoogleDataStudioAssetType assetType, String gdsId) {
         return DataStudioAsset._internal()
                 .guid("-" + ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE - 1))
-                .qualifiedName(connectionQualifiedName + "/" + name)
+                .qualifiedName(generateQualifiedName(gdsId, connectionQualifiedName))
                 .name(name)
                 .connectionQualifiedName(connectionQualifiedName)
                 .connectorType(AtlanConnectorType.DATASTUDIO)
                 .dataStudioAssetType(assetType);
+    }
+
+    /**
+     * Generate a unique DataStudioAsset name.
+     *
+     * @param gdsId unique identifier of the asset within Google Data Studio
+     * @param connectionQualifiedName unique name of the connection through which the DataStudioAsset is accessible
+     * @return a unique name for the DataStudioAsset
+     */
+    public static String generateQualifiedName(String gdsId, String connectionQualifiedName) {
+        return connectionQualifiedName + "/" + gdsId;
     }
 
     /**
--- src/main/resources/templates/DataStudioAsset.ftl
+++ src/main/resources/templates/DataStudioAsset.ftl
@@ -9,13 +9,38 @@
      */
     public static DataStudioAssetBuilder<?, ?> creator(
             String name, String connectionQualifiedName, GoogleDataStudioAssetType assetType) {
+        return DataStudioAsset.creator(name, connectionQualifiedName, assetType, UUID.randomUUID().toString());
+    }
+
+    /**
+     * Builds the minimal object necessary to create a Google Data Studio asset.
+     *
+     * @param name of the asset
+     * @param connectionQualifiedName unique name of the connection through which the asset is accessible
+     * @param assetType type of the asset
+     * @param gdsId unique identifier of the asset within Google Data Studio
+     * @return the minimal object necessary to create the asset, as a builder
+     */
+    public static DataStudioAssetBuilder<?, ?> creator(
+            String name, String connectionQualifiedName, GoogleDataStudioAssetType assetType, String gdsId) {
         return DataStudioAsset._internal()
                 .guid("-" + ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE - 1))
-                .qualifiedName(connectionQualifiedName + "/" + name)
+                .qualifiedName(generateQualifiedName(gdsId, connectionQualifiedName))
                 .name(name)
                 .connectionQualifiedName(connectionQualifiedName)
                 .connectorType(AtlanConnectorType.DATASTUDIO)
                 .dataStudioAssetType(assetType);
+    }
+
+    /**
+     * Generate a unique DataStudioAsset name.
+     *
+     * @param gdsId unique identifier of the asset within Google Data Studio
+     * @param connectionQualifiedName unique name of the connection through which the DataStudioAsset is accessible
+     * @return a unique name for the DataStudioAsset
+     */
+    public static String generateQualifiedName(String gdsId, String connectionQualifiedName) {
+        return connectionQualifiedName + "/" + gdsId;
     }
 
     /**
