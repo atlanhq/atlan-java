7f1f7ba617fc265f04acbf0b941e7eb7ee4cb6f9
Christopher Grote <cmgrote@users.noreply.github.com>
2024-01-02 22:19:10 +0000

    Adds method to send email, adds email option to admin export
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- samples/packages/admin-export/src/test/kotlin/ExportUsersTest.kt
+++ samples/packages/admin-export/src/test/kotlin/ExportUsersTest.kt
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0
    Copyright 2023 Atlan Pte. Ltd. */
 import com.atlan.pkg.PackageTest
+import com.atlan.pkg.Utils
 import com.atlan.pkg.ae.AdminExporter
 import com.atlan.pkg.serde.xls.ExcelReader
 import org.testng.Assert.assertFalse
@@ -27,6 +28,7 @@ class ExportUsersTest : PackageTest() {
             AdminExportCfg(
                 objectsToInclude = listOf("users"),
                 includeNativePolicies = false,
+                emailAddresses = null,
             ),
         )
         AdminExporter.main(arrayOf(testDirectory))
