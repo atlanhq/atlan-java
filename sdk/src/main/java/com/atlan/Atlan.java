9e1bbf69d33f8a7b986a6580fb64aa6f2b26dd30
Christopher Grote <cmgrote@users.noreply.github.com>
2024-02-19 15:28:01 +0000

    Bump to release
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- sdk/src/main/java/com/atlan/Atlan.java
+++ sdk/src/main/java/com/atlan/Atlan.java
@@ -16,7 +16,7 @@ public abstract class Atlan {
     public static final int DEFAULT_READ_TIMEOUT = 120 * 1000;
     public static final int DEFAULT_NETWORK_RETRIES = 5;
 
-    public static final String VERSION = "1.10.8-SNAPSHOT";
+    public static final String VERSION = "1.10.8";
 
     // Note: these are set here so that they can be overridden,
     // i.e. when using the SDK in a workflow setting
