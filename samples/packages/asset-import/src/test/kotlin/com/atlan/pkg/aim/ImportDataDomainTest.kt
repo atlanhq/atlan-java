3255eb7409b027a66897b21221cf7defb7f69a86
Ernest Hill <ernest@atlan.com>
2024-05-02 11:25:34 +0300

    Add additional columns

--- samples/packages/asset-import/src/test/kotlin/com/atlan/pkg/aim/ImportDataDomainTest.kt
+++ samples/packages/asset-import/src/test/kotlin/com/atlan/pkg/aim/ImportDataDomainTest.kt
@@ -4,10 +4,9 @@ package com.atlan.pkg.aim
 
 import AssetImportCfg
 import com.atlan.Atlan
-import com.atlan.model.assets.Asset
 import com.atlan.model.assets.DataDomain
 import com.atlan.model.assets.DataProduct
-import com.atlan.model.assets.IDataMesh
+import com.atlan.model.enums.AtlanAnnouncementType
 import com.atlan.model.enums.AtlanIcon
 import com.atlan.model.enums.AtlanTagColor
 import com.atlan.model.enums.CertificateStatus
@@ -30,6 +29,7 @@ class ImportDataDomainTest : PackageTest() {
     private val dataDomain3 = makeUnique("idd3")
     private val dataProduct1 = makeUnique("idp1")
     private val tag1 = makeUnique("idt1")
+    private val tag2 = makeUnique("idt2")
 
     private val testFile = "input.csv"
 
@@ -49,17 +49,20 @@ class ImportDataDomainTest : PackageTest() {
                     .replace("{{DATADOMAIN2}}", dataDomain2)
                     .replace("{{DATADOMAIN3}}", dataDomain3)
                     .replace("{{DATAPRODUCT1}}", dataProduct1)
-                    .replace("{{TAG1}}", tag1)
+//                    .replace("{{TAG1}}", tag1)
+//                    .replace("{{TAG2}}", tag1)
+                    .replace("{{TAG1}}", "")
+                    .replace("{{TAG2}}", "")
                 output.appendText("$revised\n")
             }
         }
     }
 
     private fun createTags() {
-        val maxNetworkRetries = 30
+        val maxNetworkRetries = 3
         val client = Atlan.getDefaultClient()
         val t1 = AtlanTagDef.creator(tag1, AtlanIcon.AIRPLANE, AtlanTagColor.GREEN).build()
-//        val t2 = AtlanTagDef.creator(tag2, AtlanIcon.ROBOT, AtlanTagColor.RED).build()
+        val t2 = AtlanTagDef.creator(tag2, AtlanIcon.ROBOT, AtlanTagColor.RED).build()
         client.typeDefs.create(
             listOf(t1),
             RequestOptions.from(client).maxNetworkRetries(maxNetworkRetries).build(),
@@ -79,6 +82,10 @@ class ImportDataDomainTest : PackageTest() {
         DataDomain.PARENT_DOMAIN,
         DataDomain.PARENT_DOMAIN_QUALIFIED_NAME,
         DataDomain.SUPER_DOMAIN_QUALIFIED_NAME,
+        DataDomain.ANNOUNCEMENT_TYPE,
+        DataDomain.ANNOUNCEMENT_TITLE,
+        DataDomain.ANNOUNCEMENT_MESSAGE,
+        DataDomain.README,
     )
 
     private val dataProductAttrs: List<AtlanField> = listOf(
@@ -90,6 +97,8 @@ class ImportDataDomainTest : PackageTest() {
         DataProduct.CERTIFICATE_STATUS,
         DataProduct.CERTIFICATE_STATUS_MESSAGE,
         DataProduct.DATA_DOMAIN,
+        DataProduct.ATLAN_TAGS,
+        DataProduct.README,
     )
 
     @BeforeClass
@@ -117,10 +126,7 @@ class ImportDataDomainTest : PackageTest() {
 
     @Test
     fun domain1Created() {
-        val level1_domains = DataDomain.findByName(dataDomain1, dataDomainAttrs)
-        assertNotNull(level1_domains)
-        assertEquals(level1_domains.size, 1)
-        val d1 = level1_domains[0]
+        val d1 = findDataDomain(dataDomain1)
         assertEquals(dataDomain1, d1.name)
         assertEquals(AtlanIcon.FILE_CLOUD, d1.assetIcon)
         assertEquals("#3940E1", d1.assetThemeHex)
@@ -129,11 +135,14 @@ class ImportDataDomainTest : PackageTest() {
         assertEquals(setOf("ernest"), d1.ownerUsers)
         assertEquals(setOf("admins"), d1.ownerGroups)
         assertEquals(d1.qualifiedName, DataDomain.generateQualifiedName(IDataMesh.generateSlugForName(dataDomain1), null))
-        val level2_domains = DataDomain.findByName(dataDomain2, dataDomainAttrs)
-        assertNotNull(level2_domains)
-        assertEquals(level2_domains.size, 1)
+        assertNotNull(d1.readme)
+        assertEquals("<h1>This is Domain!</h1>", d1.readme.description)
         assertEquals(CertificateStatus.VERIFIED, d1.certificateStatus)
-        val d2 = level2_domains[0]
+        assertEquals(AtlanAnnouncementType.WARNING, d1.announcementType)
+        assertEquals("Careful", d1.announcementTitle)
+        assertEquals("This is only a test.", d1.announcementMessage)
+
+        val d2 = findDataDomain(dataDomain2)
         assertEquals(dataDomain2, d2.name)
         assertEquals("Test subdomain", d2.userDescription)
         assertEquals(setOf("ernest"), d2.ownerUsers)
@@ -142,12 +151,10 @@ class ImportDataDomainTest : PackageTest() {
         assertEquals(d2.qualifiedName, DataDomain.generateQualifiedName(IDataMesh.generateSlugForName(dataDomain2), d1.qualifiedName))
         assertEquals(d2.parentDomainQualifiedName, d1.qualifiedName)
         assertEquals(d1.qualifiedName, d2.superDomainQualifiedName)
-        val level3_domains = DataDomain.findByName(dataDomain3, dataDomainAttrs)
-        assertNotNull(level3_domains)
-        assertEquals(level3_domains.size, 1)
         assertEquals(CertificateStatus.DRAFT, d2.certificateStatus)
         assertEquals("With a message!", d2.certificateStatusMessage)
-        val d3 = level3_domains[0]
+
+        val d3 = findDataDomain(dataDomain3)
         assertEquals(dataDomain3, d3.name)
         assertEquals("Test sub sub domain", d3.userDescription)
         assertEquals(setOf("ernest"), d3.ownerUsers)
@@ -156,18 +163,35 @@ class ImportDataDomainTest : PackageTest() {
         assertEquals(d3.qualifiedName, DataDomain.generateQualifiedName(IDataMesh.generateSlugForName(dataDomain3), d2.qualifiedName))
         assertEquals(d3.parentDomainQualifiedName, d2.qualifiedName)
         assertEquals(d1.qualifiedName, d3.superDomainQualifiedName)
+
         val p1 = findDataProductWithRetry(dataProduct1)
         assertEquals(dataProduct1, p1.name)
         assertEquals("Test data product", p1.userDescription)
         assertEquals(setOf("ernest"), p1.ownerUsers)
         assertEquals(setOf("admins"), p1.ownerGroups)
         assertEquals(d3.qualifiedName, p1.dataDomain.qualifiedName)
+        assertNotNull(p1.readme)
+        assertEquals("<h1>This is Product!</h1>", p1.readme.description)
+//        assertEquals(setOf(tag1, tag2), p1.atlanTags.map(AtlanTag::getTypeName).toSet())
+    }
+
+    private fun findDataDomain(domainName: String): DataDomain {
+        val request = DataDomain.select()
+            .where(DataDomain.NAME.eq(domainName))
+            .includesOnResults(dataDomainAttrs)
+            .includeOnRelations(Readme.DESCRIPTION)
+            .toRequest()
+        val response = retrySearchUntil(request, 1)
+        val dataDomains = response.stream().filter { a: Asset? -> a is DataDomain }.toList()
+        assertEquals(1, dataDomains.size)
+        return dataDomains[0] as DataDomain
     }
 
     private fun findDataProductWithRetry(productName: String): DataProduct {
         val request = DataProduct.select()
             .where(DataProduct.NAME.eq(productName))
             .includesOnResults(dataProductAttrs)
+            .includeOnRelations(Readme.DESCRIPTION)
             .toRequest()
         val response = retrySearchUntil(request, 1)
         return response.stream().filter { a: Asset? -> a is DataProduct }.findFirst().get() as DataProduct
