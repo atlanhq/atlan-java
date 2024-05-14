9699504e57eeffe3b044a8ae655b6b58a7a2f925
Christopher Grote <cmgrote@users.noreply.github.com>
2024-05-14 19:35:30 +0100

    Adds a custom package to extend custom metadata scope
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

---
+++ samples/packages/custom-metadata-extender/src/main/kotlin/CustomMetadataExtenderCfg.kt
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: Apache-2.0
+   Copyright 2024 Atlan Pte. Ltd. */
+import com.atlan.pkg.CustomConfig
+import com.atlan.pkg.serde.WidgetSerde
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import javax.annotation.processing.Generated
+
+/**
+ * Expected configuration for the Custom Metadata Extender custom package.
+ */
+@Generated("com.atlan.pkg.CustomPackage")
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+data class CustomMetadataExtenderCfg(
+    @JsonProperty("custom_metadata") val customMetadata: String? = null,
+    @JsonDeserialize(using = WidgetSerde.MultiSelectDeserializer::class)
+    @JsonSerialize(using = WidgetSerde.MultiSelectSerializer::class)
+    @JsonProperty("connection_qualified_name") val connectionQualifiedName: List<String>? = null,
+    @JsonProperty("glossaries") val glossaries: String? = null,
+) : CustomConfig()
