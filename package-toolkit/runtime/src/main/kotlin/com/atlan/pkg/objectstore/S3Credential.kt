5312fb17c13089c39fd319a7e142496606a9d39a
Christopher Grote <cmgrote@users.noreply.github.com>
2024-05-24 22:42:05 +0100

    Fixes default fallback for S3
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/S3Credential.kt
+++ package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/S3Credential.kt
@@ -9,8 +9,8 @@ import com.atlan.pkg.model.Credential
 data class S3Credential(val from: Credential) {
     val accessKey = from.username ?: ""
     val secretKey = from.password ?: ""
-    val region = Utils.getOrDefault(from.extra?.get("region") as String, getEnvVar("AWS_S3_REGION"))
-    val bucket = Utils.getOrDefault(from.extra?.get("s3_bucket") as String, getEnvVar("AWS_S3_BUCKET_NAME"))
+    val region = Utils.getOrDefault((from.extra?.get("region") ?: "") as String, getEnvVar("AWS_S3_REGION"))
+    val bucket = Utils.getOrDefault((from.extra?.get("s3_bucket") ?: "") as String, getEnvVar("AWS_S3_BUCKET_NAME"))
     val objectPrefix = (from.extra?.get("s3_prefix") ?: "") as String
     val objectKey = (from.extra?.get("s3_key") ?: "") as String
 }
