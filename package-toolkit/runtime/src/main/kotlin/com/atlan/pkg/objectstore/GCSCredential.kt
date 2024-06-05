1821624b711b5b2501165366605f0d2196f68a6a
Christopher Grote <cmgrote@users.noreply.github.com>
2024-06-05 16:57:34 +0100

    Attempt reasonable fallbacks for backing object stores
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/GCSCredential.kt
+++ package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/GCSCredential.kt
@@ -2,12 +2,20 @@
    Copyright 2023 Atlan Pte. Ltd. */
 package com.atlan.pkg.objectstore
 
+import com.atlan.pkg.Utils
+import com.atlan.pkg.Utils.getEnvVar
 import com.atlan.pkg.model.Credential
 
+/**
+ * Credentials captured for GCS connectivity.
+ */
 data class GCSCredential(val from: Credential) {
-    val projectId = from.username ?: ""
+    /** GCP project ID provided, or the default project for Atlan's backing store if none was provided. */
+    val projectId = Utils.getOrDefault(from.username ?: "", getEnvVar("GCP_PROJECT_ID"))
     val serviceAccountJson = from.password ?: ""
-    val bucket = (from.extra?.get("gcs_bucket") ?: "") as String
+
+    /** Bucket provided, or the default bucket for Atlan's backing store if none was provided. */
+    val bucket = Utils.getOrDefault((from.extra?.get("gcs_bucket") ?: "") as String, getEnvVar("GCP_STORAGE_BUCKET"))
     val objectPrefix = (from.extra?.get("gcs_prefix") ?: "") as String
     val objectKey = (from.extra?.get("gcs_key") ?: "") as String
 }
--- package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/S3Credential.kt
+++ package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/S3Credential.kt
@@ -6,10 +6,17 @@ import com.atlan.pkg.Utils
 import com.atlan.pkg.Utils.getEnvVar
 import com.atlan.pkg.model.Credential
 
+/**
+ * Credentials captured for S3 connectivity.
+ */
 data class S3Credential(val from: Credential) {
     val accessKey = from.username ?: ""
     val secretKey = from.password ?: ""
+
+    /** Region provided, or the region of Atlan's backing store if none was provided. */
     val region = Utils.getOrDefault((from.extra?.get("region") ?: "") as String, getEnvVar("AWS_S3_REGION"))
+
+    /** Bucket provided, or the default bucket for Atlan's backing store if none was provided. */
     val bucket = Utils.getOrDefault((from.extra?.get("s3_bucket") ?: "") as String, getEnvVar("AWS_S3_BUCKET_NAME"))
     val objectPrefix = (from.extra?.get("s3_prefix") ?: "") as String
     val objectKey = (from.extra?.get("s3_key") ?: "") as String
