58453d76e6109bcf18bbf3973b0c701f32d0217a
Christopher Grote <cmgrote@users.noreply.github.com>
2024-06-27 18:30:54 +0100

    Abstracts OpenLineage for Spark-based representation
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

---
+++ sdk/src/main/java/com/atlan/model/lineage/OpenLineageDataset.java
@@ -0,0 +1,107 @@
+/* SPDX-License-Identifier: Apache-2.0
+   Copyright 2022 Atlan Pte. Ltd. */
+package com.atlan.model.lineage;
+
+import com.atlan.AtlanClient;
+import com.atlan.model.core.AtlanObject;
+import io.openlineage.client.OpenLineage;
+import io.openlineage.client.OpenLineageClientUtils;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Base class for handling OpenLineage datasets, passing through to the OpenLineage Java SDK
+ * but wrapping datasets such that they are handled appropriately in the Atlan Java SDK.
+ */
+@SuperBuilder(toBuilder = true, builderMethodName = "_internal")
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+public class OpenLineageDataset extends AtlanObject {
+    private static final long serialVersionUID = 2L;
+
+    /** Internal definition of the OpenLineage dataset. */
+    @Getter(AccessLevel.PACKAGE)
+    OpenLineage.Dataset _dataset;
+
+    /** Producer definition for OpenLineage. */
+    protected OpenLineage openLineage;
+
+    /**
+     * Name of the source where the dataset exists.
+     * See: https://github.com/OpenLineage/OpenLineage/blob/main/spec/Naming.md
+     */
+    private String namespace;
+
+    /**
+     * Name of the asset, by OpenLineage standards.
+     * For example: DB.SCHEMA.TABLE
+     */
+    private String assetName;
+
+    /**
+     * Builds the minimal object necessary to create an OpenLineage dataset.
+     * Note: before you can use the dataset, you will need to wire it as either an input or output
+     * using either {@code toInput()} or {@code toOutput()}.
+     *
+     * @param namespace name of the source of the asset (see: https://github.com/OpenLineage/OpenLineage/blob/main/spec/Naming.md)
+     * @param assetName name of the asset, by OpenLineage standard (for example, DB.SCHEMA.TABLE)
+     * @param producer a pre-configured OpenLineage producer
+     * @return the minimal request necessary to create the dataset, as a builder
+     */
+    static OpenLineageDatasetBuilder<?, ?> creator(String namespace, String assetName, OpenLineage producer) {
+        return _internal().openLineage(producer).namespace(namespace).assetName(assetName);
+    }
+
+    // TODO: provide some intuitive way to manage the facets of the dataset
+
+    // TODO: provide some way to manage a schema, i.e. for partial assets
+    // NOTE: If we want a structured partial asset, then we need the following lines, too
+    //  (not needed for a "full" asset as they'll already be in Atlan)
+    /*.dataSource(ol.newDatasourceDatasetFacetBuilder()
+        .name("snowflake://qia75894.snowflakecomputing.com")
+        .uri(URI.create("snowflake://qia75894.snowflakecomputing.com"))
+        .build())
+    .schema(ol.newSchemaDatasetFacetBuilder()
+        .fields(listOf(
+            ol.newSchemaDatasetFacetFieldsBuilder().name("ID").type("VARCHAR").build(),
+            ol.newSchemaDatasetFacetFieldsBuilder().name("PARENT_ID").type("VARCHAR").build(),
+            ol.newSchemaDatasetFacetFieldsBuilder().name("ORG_WIDE_EMAIL_ADDRESS_ID").type("VARCHAR").build(),
+            ol.newSchemaDatasetFacetFieldsBuilder().name("SYSTEM_MODSTAMP").type("TIMESTAMPTZ").build(),
+            ol.newSchemaDatasetFacetFieldsBuilder().name("_FIVETRAN_DELETED").type("BOOLEAN").build(),
+            ol.newSchemaDatasetFacetFieldsBuilder().name("_FIVETRAN_SYNCED").type("TIMESTAMPTZ").build(),
+        ))
+        .build())
+    .build())*/
+
+    /** {@inheritDoc} */
+    @Override
+    public String toJson(AtlanClient client) {
+        return OpenLineageClientUtils.toJson(get_dataset());
+    }
+
+    public abstract static class OpenLineageDatasetBuilder<
+                    C extends OpenLineageDataset, B extends OpenLineageDataset.OpenLineageDatasetBuilder<C, B>>
+            extends AtlanObject.AtlanObjectBuilder<C, B> {
+
+        /**
+         * Create an input dataset from this builder.
+         *
+         * @return the dataset as an input dataset.
+         */
+        public OpenLineageInputDataset.OpenLineageInputDatasetBuilder<?, ?> toInput() {
+            return OpenLineageInputDataset.creator(namespace, assetName, openLineage);
+        }
+
+        /**
+         * Create an output dataset from this builder.
+         *
+         * @return the dataset as an output dataset.
+         */
+        public OpenLineageOutputDataset.OpenLineageOutputDatasetBuilder<?, ?> toOutput() {
+            return OpenLineageOutputDataset.creator(namespace, assetName, openLineage);
+        }
+    }
+}
---
+++ sdk/src/main/java/com/atlan/model/lineage/OpenLineageOutputDataset.java
@@ -0,0 +1,87 @@
+/* SPDX-License-Identifier: Apache-2.0
+   Copyright 2022 Atlan Pte. Ltd. */
+package com.atlan.model.lineage;
+
+import io.openlineage.client.OpenLineage;
+import java.util.List;
+import java.util.Map;
+import lombok.EqualsAndHashCode;
+import lombok.Singular;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Base class for handling OpenLineage datasets to be used as lineage targets (outputs),
+ * passing through to the OpenLineage Java SDK but wrapping datasets such that they are
+ * handled appropriately in the Atlan Java SDK.
+ */
+@SuperBuilder(toBuilder = true, builderMethodName = "_internal")
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+@SuppressWarnings("cast")
+public class OpenLineageOutputDataset extends OpenLineageDataset {
+    private static final long serialVersionUID = 2L;
+
+    private OpenLineage.OutputDatasetBuilder _builder;
+
+    /**
+     * Column-level lineage.
+     * Each entry should be keyed by the name of a column in this output dataset, and the value
+     * should be a list of all input datasets' fields that are used as input into the output column.
+     */
+    @Singular
+    Map<String, List<OpenLineage.ColumnLineageDatasetFacetFieldsAdditionalInputFields>> toFields;
+
+    /**
+     * Builds the minimal object necessary to create an OpenLineage dataset use-able as a lineage target.
+     *
+     * @param namespace name of the source of the asset (see: https://github.com/OpenLineage/OpenLineage/blob/main/spec/Naming.md)
+     * @param assetName name of the asset, by OpenLineage standard (for example, DB.SCHEMA.TABLE)
+     * @param producer a pre-configured OpenLineage producer
+     * @return the minimal request necessary to create the job, as a builder
+     */
+    static OpenLineageOutputDatasetBuilder<?, ?> creator(String namespace, String assetName, OpenLineage producer) {
+        return _internal()
+                .openLineage(producer)
+                ._builder(producer.newOutputDatasetBuilder()
+                        .namespace(namespace)
+                        .name(assetName)
+                        .facets(producer.newDatasetFacetsBuilder().build()));
+    }
+
+    // TODO: provide some intuitive way to manage the facets of the dataset
+
+    /**
+     * Retrieve the OutputDataset in OpenLineage standard form.
+     *
+     * @return the OpenLineage standard for the OutputDataset
+     */
+    @Override
+    OpenLineage.OutputDataset get_dataset() {
+        if (toFields == null || toFields.isEmpty()) {
+            return this._builder.build();
+        } else {
+            OpenLineage.ColumnLineageDatasetFacetFieldsBuilder fieldsBuilder =
+                    openLineage.newColumnLineageDatasetFacetFieldsBuilder();
+            for (Map.Entry<String, List<OpenLineage.ColumnLineageDatasetFacetFieldsAdditionalInputFields>> entry :
+                    toFields.entrySet()) {
+                fieldsBuilder.put(
+                        entry.getKey(),
+                        openLineage
+                                .newColumnLineageDatasetFacetFieldsAdditionalBuilder()
+                                .inputFields(entry.getValue())
+                                .build());
+            }
+            // TODO: below code will clobber any pre-existing facets
+            return this._builder
+                    .facets(openLineage
+                            .newDatasetFacetsBuilder()
+                            .columnLineage(openLineage
+                                    .newColumnLineageDatasetFacetBuilder()
+                                    .fields(fieldsBuilder.build())
+                                    .build())
+                            .build())
+                    .build();
+        }
+    }
+}
