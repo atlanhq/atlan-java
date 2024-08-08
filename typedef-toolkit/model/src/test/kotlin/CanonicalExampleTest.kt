491bfcb050a15ba7969e447229578a2f69f1cdbb
Christopher Grote <cmgrote@users.noreply.github.com>
2024-08-08 17:43:23 +0100

    Fix tests
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- typedef-toolkit/model/src/test/kotlin/CanonicalExampleTest.kt
+++ typedef-toolkit/model/src/test/kotlin/CanonicalExampleTest.kt
@@ -47,16 +47,16 @@ object CanonicalExampleTest {
         assertEquals(1, model.shared.supertypeDefinition.superTypes.size)
         assertEquals("Catalog", model.shared.supertypeDefinition.superTypes[0])
         assertNotNull(model.shared.supertypeDefinition.attributeDefs)
-        assertEquals(3, model.shared.supertypeDefinition.attributeDefs.size)
-        assertEquals("customSourceId", model.shared.supertypeDefinition.attributeDefs[0].name)
-        assertEquals("Unique identifier for the Custom asset from the source system.", model.shared.supertypeDefinition.attributeDefs[0].description)
-        assertEquals("string", model.shared.supertypeDefinition.attributeDefs[0].typeName)
-        assertEquals("customDatasetName", model.shared.supertypeDefinition.attributeDefs[1].name)
-        assertEquals("Simple name of the dataset in which this asset exists, or empty if it is itself a dataset.", model.shared.supertypeDefinition.attributeDefs[1].description)
-        assertEquals("string", model.shared.supertypeDefinition.attributeDefs[1].typeName)
-        assertEquals("customDatasetQualifiedName", model.shared.supertypeDefinition.attributeDefs[2].name)
-        assertEquals("Unique name of the dataset in which this asset exists, or empty if it is itself a dataset.", model.shared.supertypeDefinition.attributeDefs[2].description)
-        assertEquals("string", model.shared.supertypeDefinition.attributeDefs[2].typeName)
+        assertEquals(3, model.shared.supertypeDefinition.attributeDefs!!.size)
+        assertEquals("customSourceId", model.shared.supertypeDefinition.attributeDefs!![0].name)
+        assertEquals("Unique identifier for the Custom asset from the source system.", model.shared.supertypeDefinition.attributeDefs!![0].description)
+        assertEquals("string", model.shared.supertypeDefinition.attributeDefs!![0].typeName)
+        assertEquals("customDatasetName", model.shared.supertypeDefinition.attributeDefs!![1].name)
+        assertEquals("Simple name of the dataset in which this asset exists, or empty if it is itself a dataset.", model.shared.supertypeDefinition.attributeDefs!![1].description)
+        assertEquals("string", model.shared.supertypeDefinition.attributeDefs!![1].typeName)
+        assertEquals("customDatasetQualifiedName", model.shared.supertypeDefinition.attributeDefs!![2].name)
+        assertEquals("Unique name of the dataset in which this asset exists, or empty if it is itself a dataset.", model.shared.supertypeDefinition.attributeDefs!![2].description)
+        assertEquals("string", model.shared.supertypeDefinition.attributeDefs!![2].typeName)
     }
 
     @Test
@@ -74,7 +74,7 @@ object CanonicalExampleTest {
         assertEquals("Instances of CustomDataset in Atlan.", dataset.description)
         assertEquals(1, dataset.superTypes.size)
         assertEquals("Custom", dataset.superTypes[0])
-        assertTrue(dataset.attributeDefs.isEmpty())
+        assertTrue(dataset.attributeDefs!!.isEmpty())
     }
 
     @Test
@@ -86,10 +86,10 @@ object CanonicalExampleTest {
         assertEquals(2, table.superTypes.size)
         assertEquals("Custom", table.superTypes[0])
         assertEquals("Table", table.superTypes[1])
-        assertEquals(1, table.attributeDefs.size)
-        assertEquals("customRatings", table.attributeDefs[0].name)
-        assertEquals("Ratings for the CustomTable asset from the source system.", table.attributeDefs[0].description)
-        assertEquals("array<CustomRatings>", table.attributeDefs[0].typeName)
+        assertEquals(1, table.attributeDefs!!.size)
+        assertEquals("customRatings", table.attributeDefs!![0].name)
+        assertEquals("Ratings for the CustomTable asset from the source system.", table.attributeDefs!![0].description)
+        assertEquals("array<CustomRatings>", table.attributeDefs!![0].typeName)
     }
 
     @Test
@@ -104,13 +104,13 @@ object CanonicalExampleTest {
         assertNotNull(ratings)
         assertEquals("CustomRatings", ratings.name)
         assertEquals("Ratings for an asset from the source system.", ratings.description)
-        assertEquals(2, ratings.attributeDefs.size)
-        assertEquals("customRatingFrom", ratings.attributeDefs[0].name)
-        assertEquals("Username of the user who left the rating.", ratings.attributeDefs[0].description)
-        assertEquals("string", ratings.attributeDefs[0].typeName)
-        assertEquals("customRatingOf", ratings.attributeDefs[1].name)
-        assertEquals("Numeric score for the rating left by the user.", ratings.attributeDefs[1].description)
-        assertEquals("long", ratings.attributeDefs[1].typeName)
+        assertEquals(2, ratings.attributeDefs!!.size)
+        assertEquals("customRatingFrom", ratings.attributeDefs!![0].name)
+        assertEquals("Username of the user who left the rating.", ratings.attributeDefs!![0].description)
+        assertEquals("string", ratings.attributeDefs!![0].typeName)
+        assertEquals("customRatingOf", ratings.attributeDefs!![1].name)
+        assertEquals("Numeric score for the rating left by the user.", ratings.attributeDefs!![1].description)
+        assertEquals("long", ratings.attributeDefs!![1].typeName)
     }
 
     @Test
@@ -122,10 +122,10 @@ object CanonicalExampleTest {
         assertEquals(2, field.superTypes.size)
         assertEquals("Custom", field.superTypes[0])
         assertEquals("Column", field.superTypes[1])
-        assertEquals(1, field.attributeDefs.size)
-        assertEquals("customTemperature", field.attributeDefs[0].name)
-        assertEquals("Temperature of the CustomTable asset.", field.attributeDefs[0].description)
-        assertEquals("CustomTemperatureType", field.attributeDefs[0].typeName)
+        assertEquals(1, field.attributeDefs!!.size)
+        assertEquals("customTemperature", field.attributeDefs!![0].name)
+        assertEquals("Temperature of the CustomTable asset.", field.attributeDefs!![0].description)
+        assertEquals("CustomTemperatureType", field.attributeDefs!![0].typeName)
     }
 
     @Test
