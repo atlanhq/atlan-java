12da5646815dee8bc71c0b9e78536af4a1a5e38e
Christopher Grote <cmgrote@users.noreply.github.com>
2024-10-07 15:12:51 +0100

    Linting fixes
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- sdk/src/main/java/com/atlan/net/HttpClient.java
+++ sdk/src/main/java/com/atlan/net/HttpClient.java
@@ -237,13 +237,13 @@ public abstract class HttpClient {
         if (numRetries >= request.options().getMaxNetworkRetries()) {
             if (exception != null) {
                 log.error(
-                    " ... beyond max retries ({}), failing! If this is unexpected, you can try increasing the maximum retries through Atlan.setMaxNetworkRetries()",
-                    request.options().getMaxNetworkRetries(),
-                    exception);
+                        " ... beyond max retries ({}), failing! If this is unexpected, you can try increasing the maximum retries through Atlan.setMaxNetworkRetries()",
+                        request.options().getMaxNetworkRetries(),
+                        exception);
             } else {
                 log.error(
-                    " ... beyond max retries ({}), failing! If this is unexpected, you can try increasing the maximum retries through Atlan.setMaxNetworkRetries()",
-                    request.options().getMaxNetworkRetries());
+                        " ... beyond max retries ({}), failing! If this is unexpected, you can try increasing the maximum retries through Atlan.setMaxNetworkRetries()",
+                        request.options().getMaxNetworkRetries());
             }
             return false;
         }
