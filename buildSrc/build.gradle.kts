2606e310d0632da098b1976d1d05ba0ee42b3664
Christopher Grote <cmgrote@users.noreply.github.com>
2024-10-07 20:37:49 +0100

    Dependency bumps
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.0.20")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
-    implementation("io.freefair.gradle:lombok-plugin:8.10")
+    implementation("io.freefair.gradle:lombok-plugin:8.10.2")
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:4.0.1")
     implementation("com.adarshr:gradle-test-logger-plugin:4.0.0")
     implementation("org.pkl-lang:org.pkl-lang.gradle.plugin:0.26.3")
