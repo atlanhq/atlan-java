19c7f8286063fd517d31aa676fde4ff33a562527
Ernest Hill <ernest@atlan.com>
2024-10-16 15:54:21 +0300

    Fix typo

--- package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/S3Credential.kt
+++ package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/objectstore/S3Credential.kt
@@ -19,9 +19,9 @@ data class S3Credential(val from: Credential) {
     /** Bucket provided, or the default bucket for Atlan's backing store if none was provided. */
     val bucket = Utils.getOrDefault((from.extra?.get("bucket") ?: "") as String, Utils.getOrDefault((from.extra?.get("s3_bucket") ?: "") as String, getEnvVar("AWS_S3_BUCKET_NAME")))
 
-    /** Prefix provided or null */
+    /** Prefix provided or empty string */
     val prefix = (from.extra?.get("prefix") ?: "") as String
 
-    /** Role ARN or null */
+    /** Role ARN or empty string */
     val roleArn = (from.extra?.get("aws_role_arn") ?: "") as String
 }
