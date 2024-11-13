092f705a168c19dba0bc9d38656666a22acc01b7
prateekrai-atlan <prateek.rai@atlan.com>
2024-11-13 19:28:47 +0530

    `application asset` builder method

--- sdk/src/main/java/com/atlan/model/assets/ApplicationAsset.java
+++ sdk/src/main/java/com/atlan/model/assets/ApplicationAsset.java
@@ -399,6 +399,60 @@ public  class ApplicationAsset extends Asset implements IApplicationAsset, IAppl
     }
 
     /**
+     * Builds the minimal object necessary to create a Application_Asset asset.
+     *
+     * @param name of the application asset
+     * @param connectionQualifiedName unique name of the connection through which the application asset is accessible
+     * @return the minimal object necessary to create the application asset, as a builder
+     */
+    public static ApplicationAsset.ApplicationAssetBuilder<?, ?> creator(String name, String connectionQualifiedName) {
+        return ApplicationAsset._internal()
+            .guid("-" + ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE - 1))
+            .qualifiedName(connectionQualifiedName + "/" + name)
+            .name(name)
+            .connectionQualifiedName(connectionQualifiedName)
+            .connectorType(AtlanConnectorType.APPLICATION);
+    }
+
+    /**
+     * Builds the minimal object necessary to create a Application_Asset asset.
+     *
+     * @param name of the application asset
+     * @param connectionQualifiedName unique name of the connection through which the application asset is accessible
+     * @param applicationAssetId id of the application asset in the source system
+     * @return the minimal object necessary to create the application asset, as a builder
+     */
+    public static ApplicationAsset.ApplicationAssetBuilder<?, ?> creator(String name, String connectionQualifiedName, String applicationAssetId) {
+        return ApplicationAsset._internal()
+            .guid("-" + ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE - 1))
+            .qualifiedName(connectionQualifiedName + "/" + name)
+            .name(name)
+            .connectionQualifiedName(connectionQualifiedName)
+            .applicationId(applicationAssetId)
+            .connectorType(AtlanConnectorType.APPLICATION);
+    }
+
+    /**
+     * Builds the minimal object necessary to create a Application_Asset asset.
+     *
+     * @param name of the application asset
+     * @param connectionQualifiedName unique name of the connection through which the application asset is accessible
+     * @param applicationAssetId id of the application asset in the source system
+     * @param applicationCatalogList list for catalog assets contained in this application asset
+     * @return the minimal object necessary to create the application asset, as a builder
+     */
+    public static ApplicationAsset.ApplicationAssetBuilder<?, ?> creator(String name, String connectionQualifiedName, String applicationAssetId, SortedSet<ICatalog> applicationCatalogList) {
+        return ApplicationAsset._internal()
+            .guid("-" + ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE - 1))
+            .qualifiedName(connectionQualifiedName + "/" + name)
+            .name(name)
+            .connectionQualifiedName(connectionQualifiedName)
+            .applicationId(applicationAssetId)
+            .applicationCatalog(applicationCatalogList)
+            .connectorType(AtlanConnectorType.APPLICATION);
+    }
+
+    /**
      * Builds the minimal object necessary to update a ApplicationAsset.
      *
      * @param qualifiedName of the ApplicationAsset
