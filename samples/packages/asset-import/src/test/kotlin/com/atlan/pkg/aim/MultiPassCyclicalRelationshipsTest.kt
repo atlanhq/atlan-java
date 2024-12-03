5f5259bef803c1a8770e94cf6a5ee68a3507b49e
Christopher Grote <cmgrote@users.noreply.github.com>
2024-12-03 12:42:43 +0000

    Temporary fix to observe GitHub Action flow
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- samples/packages/asset-import/src/test/kotlin/com/atlan/pkg/aim/MultiPassCyclicalRelationshipsTest.kt
+++ samples/packages/asset-import/src/test/kotlin/com/atlan/pkg/aim/MultiPassCyclicalRelationshipsTest.kt
@@ -181,7 +181,7 @@ class MultiPassCyclicalRelationshipsTest : PackageTest("mpcr") {
             val to = response.assets.flatMap { (it as ModelEntity).modelEntityMappedToEntities }.filterNotNull().toSet()
             val from = response.assets.flatMap { (it as ModelEntity).modelEntityMappedFromEntities }.filterNotNull().toSet()
             count++
-        } while ((to.isEmpty() || from.isEmpty()) && count < client.maxNetworkRetries)
+        } while (to.isEmpty() || from.isEmpty()) // && count < client.maxNetworkRetries)
         assertNotNull(response)
         assertEquals(2, response.assets.size)
         assertEquals(setOf(ModelEntity.TYPE_NAME), response.assets.map { it.typeName }.toSet())
