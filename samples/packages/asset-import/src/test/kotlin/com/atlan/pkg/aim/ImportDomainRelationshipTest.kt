5ccfbeaac343f43e9953b548c743beffe4ab33b5
Pavan Manish <pavanmanishd@gmail.com>
2025-01-10 08:44:15 +0530

    ImportDomainRelationShip Test initial

--- samples/packages/asset-import/src/test/kotlin/com/atlan/pkg/aim/ImportDomainRelationshipTest.kt
+++ samples/packages/asset-import/src/test/kotlin/com/atlan/pkg/aim/ImportDomainRelationshipTest.kt
@@ -1,4 +1,25 @@
+/* SPDX-License-Identifier: Apache-2.0
+   Copyright 2025 Atlan Pte. Ltd. */
 package com.atlan.pkg.aim
 
-class ImportDomainRelationshipTest {
+import com.atlan.pkg.PackageTest
+import com.atlan.pkg.Utils
+
+class ImportDomainRelationshipTest : PackageTest("idr") {
+    override val logger = Utils.getLogger(this.javaClass.name)
+
+    private val testFile = "input.csv"
+    private val files =
+        listOf(
+            testFile,
+            "debug.log",
+        )
+
+    override fun setup() {
+        TODO("Not yet implemented")
+    }
+
+    override fun teardown() {
+        TODO("Not yet implemented")
+    }
 }
