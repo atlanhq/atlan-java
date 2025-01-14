d967cb9e98ff5bd7608f7236dde051e25eb3179b
Christopher Grote <cmgrote@users.noreply.github.com>
2025-01-14 15:20:27 +0000

    Fixes latest integration tests
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- integration-tests/src/test/java/com/atlan/java/sdk/CustomAssetTest.java
+++ integration-tests/src/test/java/com/atlan/java/sdk/CustomAssetTest.java
@@ -9,6 +9,7 @@ import com.atlan.exception.AtlanException;
 import com.atlan.model.assets.*;
 import com.atlan.model.core.AssetMutationResponse;
 import com.atlan.model.enums.AtlanConnectorType;
+import com.atlan.model.enums.AtlanIcon;
 import com.atlan.model.enums.AtlanStatus;
 import com.atlan.model.search.AggregationBucketResult;
 import com.atlan.model.search.IndexSearchRequest;
@@ -27,10 +28,14 @@ public class CustomAssetTest extends AtlanLiveTest {
 
     public static final AtlanConnectorType CONNECTOR_TYPE = AtlanConnectorType.CUSTOM;
     public static final String CONNECTION_NAME = PREFIX;
-    private static final String ENTITY_NAME = PREFIX + "-entity";
+    private static final String PARENT_NAME = PREFIX + "-parent";
+    private static final String CHILD_NAME1 = PREFIX + "-child1";
+    private static final String CHILD_NAME2 = PREFIX + "-child2";
 
     private static Connection connection = null;
-    private static CustomEntity entity = null;
+    private static CustomEntity parent = null;
+    private static CustomEntity child1 = null;
+    private static CustomEntity child2 = null;
 
     @Test(groups = {"custom.create.connection"})
     void createConnection() throws AtlanException, InterruptedException {
@@ -38,33 +43,72 @@ public class CustomAssetTest extends AtlanLiveTest {
     }
 
     @Test(
-            groups = {"custom.create.entity"},
+            groups = {"custom.create.parent"},
             dependsOnGroups = {"custom.create.connection"})
-    void createEntity() throws AtlanException {
-        CustomEntity toCreate =
-                CustomEntity.creator(ENTITY_NAME, connection.getQualifiedName()).build();
+    void createParent() throws AtlanException {
+        CustomEntity toCreate = CustomEntity.creator(PARENT_NAME, connection.getQualifiedName())
+                .subType("Guacamole")
+                .build();
         AssetMutationResponse response = toCreate.save(client);
         Asset one = validateSingleCreate(response);
         assertTrue(one instanceof CustomEntity);
-        entity = (CustomEntity) one;
-        assertNotNull(entity.getGuid());
-        assertNotNull(entity.getQualifiedName());
-        assertEquals(entity.getName(), ENTITY_NAME);
-        assertEquals(entity.getConnectorType(), CONNECTOR_TYPE);
-        assertEquals(entity.getConnectionQualifiedName(), connection.getQualifiedName());
+        parent = (CustomEntity) one;
+        assertNotNull(parent.getGuid());
+        assertNotNull(parent.getQualifiedName());
+        assertEquals(parent.getName(), PARENT_NAME);
+        assertEquals(parent.getConnectorType(), CONNECTOR_TYPE);
+        assertEquals(parent.getConnectionQualifiedName(), connection.getQualifiedName());
     }
 
     @Test(
-            groups = {"custom.update.entity"},
-            dependsOnGroups = {"custom.create.entity"})
-    void updateEntity() throws AtlanException {
+            groups = {"custom.create.children"},
+            dependsOnGroups = {"custom.create.parent"})
+    void createChildren() throws AtlanException {
+        CustomEntity one = CustomEntity.creator(CHILD_NAME1, connection.getQualifiedName())
+                .assetIcon(AtlanIcon.AVOCADO) // TODO: needs to be capable of being a URL...
+                .subType("Avocado")
+                .customParentEntity(CustomEntity.refByGuid(parent.getGuid()))
+                .build();
+        CustomEntity two = CustomEntity.creator(CHILD_NAME2, connection.getQualifiedName())
+                .assetIcon(AtlanIcon.PEPPER) // TODO: needs to be capable of being a URL...
+                .subType("Pepper")
+                .customParentEntity(CustomEntity.refByQualifiedName(parent.getQualifiedName()))
+                .customRelatedToEntity(CustomEntity.refByGuid(one.getGuid()))
+                .build();
+        AssetMutationResponse response = client.assets.save(List.of(one, two), false);
+        assertNotNull(response);
+        assertNotNull(response.getUpdatedAssets());
+        assertEquals(response.getUpdatedAssets().size(), 1);
+        assertEquals(response.getUpdatedAssets().get(0).getGuid(), parent.getGuid());
+        assertNotNull(response.getCreatedAssets());
+        assertEquals(response.getCreatedAssets().size(), 2);
+        for (Asset asset : response.getCreatedAssets()) {
+            assertTrue(asset instanceof CustomEntity);
+            assertNotNull(asset.getGuid());
+            assertNotNull(asset.getQualifiedName());
+            assertEquals(asset.getConnectorType(), CONNECTOR_TYPE);
+            assertEquals(asset.getConnectionQualifiedName(), connection.getQualifiedName());
+            if (asset.getName().equals(CHILD_NAME1)) {
+                child1 = (CustomEntity) asset;
+            } else if (asset.getName().equals(CHILD_NAME2)) {
+                child2 = (CustomEntity) asset;
+            } else {
+                throw new IllegalStateException("Created entity did not match either we attempted to create.");
+            }
+        }
+    }
+
+    @Test(
+            groups = {"custom.update.parent"},
+            dependsOnGroups = {"custom.create.*"})
+    void updateParent() throws AtlanException {
         CustomEntity updated = CustomEntity.updateCertificate(
-                client, entity.getQualifiedName(), CERTIFICATE_STATUS, CERTIFICATE_MESSAGE);
+                client, parent.getQualifiedName(), CERTIFICATE_STATUS, CERTIFICATE_MESSAGE);
         assertNotNull(updated);
         assertEquals(updated.getCertificateStatus(), CERTIFICATE_STATUS);
         assertEquals(updated.getCertificateStatusMessage(), CERTIFICATE_MESSAGE);
         updated = CustomEntity.updateAnnouncement(
-                client, entity.getQualifiedName(), ANNOUNCEMENT_TYPE, ANNOUNCEMENT_TITLE, ANNOUNCEMENT_MESSAGE);
+                client, parent.getQualifiedName(), ANNOUNCEMENT_TYPE, ANNOUNCEMENT_TITLE, ANNOUNCEMENT_MESSAGE);
         assertNotNull(updated);
         assertEquals(updated.getAnnouncementType(), ANNOUNCEMENT_TYPE);
         assertEquals(updated.getAnnouncementTitle(), ANNOUNCEMENT_TITLE);
@@ -72,21 +116,42 @@ public class CustomAssetTest extends AtlanLiveTest {
     }
 
     @Test(
-            groups = {"custom.read.entity"},
-            dependsOnGroups = {"custom.create.*", "custom.update.entity"})
-    void retrieveEntity() throws AtlanException {
-        CustomEntity c = CustomEntity.get(client, entity.getGuid(), true);
+            groups = {"custom.read.parent"},
+            dependsOnGroups = {"custom.create.*", "custom.update.parent"})
+    void retrieveParent() throws AtlanException {
+        CustomEntity c = CustomEntity.get(client, parent.getGuid(), true);
         assertNotNull(c);
         assertTrue(c.isComplete());
-        assertEquals(c.getGuid(), entity.getGuid());
-        assertEquals(c.getQualifiedName(), entity.getQualifiedName());
-        assertEquals(c.getName(), ENTITY_NAME);
+        assertEquals(c.getGuid(), parent.getGuid());
+        assertEquals(c.getQualifiedName(), parent.getQualifiedName());
+        assertEquals(c.getName(), PARENT_NAME);
         assertEquals(c.getCertificateStatus(), CERTIFICATE_STATUS);
+        assertEquals(c.getCustomChildEntities().size(), 2);
+    }
+
+    @Test(
+            groups = {"custom.read.child1"},
+            dependsOnGroups = {"custom.create.*", "custom.update.parent"})
+    void retrieveChild1() throws AtlanException {
+        CustomEntity c = CustomEntity.get(client, child1.getGuid(), true);
+        assertNotNull(c);
+        assertTrue(c.isComplete());
+        assertEquals(c.getGuid(), child1.getGuid());
+        assertEquals(c.getQualifiedName(), child1.getQualifiedName());
+        assertEquals(c.getName(), CHILD_NAME1);
+        assertTrue(
+                c.getCustomChildEntities() == null || c.getCustomChildEntities().isEmpty());
+        assertNotNull(c.getCustomParentEntity());
+        assertEquals(c.getCustomParentEntity().getGuid(), parent.getGuid());
+        assertFalse(c.getCustomRelatedFromEntities() == null
+                || c.getCustomRelatedFromEntities().isEmpty());
+        assertEquals(c.getCustomRelatedFromEntities().size(), 1);
+        assertEquals(c.getCustomRelatedFromEntities().first().getGuid(), child2.getGuid());
     }
 
     @Test(
             groups = {"custom.search.assets"},
-            dependsOnGroups = {"custom.read.entity"})
+            dependsOnGroups = {"custom.read.*"})
     void searchAssets() throws AtlanException, InterruptedException {
         IndexSearchRequest index = client.assets
                 .select()
@@ -95,11 +160,12 @@ public class CustomAssetTest extends AtlanLiveTest {
                 .pageSize(10)
                 .aggregate("type", IReferenceable.TYPE_NAME.bucketBy())
                 .sort(Asset.CREATE_TIME.order(SortOrder.Asc))
+                .sort(Asset.NAME.order(SortOrder.Asc))
                 .includeOnResults(Asset.NAME)
                 .includeOnResults(Asset.CONNECTION_QUALIFIED_NAME)
                 .toRequest();
 
-        IndexSearchResponse response = retrySearchUntil(index, 9L);
+        IndexSearchResponse response = retrySearchUntil(index, 3L);
 
         assertNotNull(response.getAggregations());
         assertEquals(response.getAggregations().size(), 1);
@@ -108,7 +174,7 @@ public class CustomAssetTest extends AtlanLiveTest {
                 ((AggregationBucketResult) response.getAggregations().get("type"))
                         .getBuckets()
                         .size(),
-                3);
+                1);
 
         assertEquals(response.getApproximateCount().longValue(), 3L);
         List<Asset> entities = response.getAssets();
@@ -119,16 +185,32 @@ public class CustomAssetTest extends AtlanLiveTest {
         assertTrue(one instanceof CustomEntity);
         assertFalse(one.isComplete());
         CustomEntity d = (CustomEntity) one;
-        assertEquals(d.getQualifiedName(), entity.getQualifiedName());
-        assertEquals(d.getName(), entity.getName());
+        assertEquals(d.getQualifiedName(), parent.getQualifiedName());
+        assertEquals(d.getName(), parent.getName());
+        assertEquals(d.getConnectionQualifiedName(), connection.getQualifiedName());
+
+        one = entities.get(1);
+        assertTrue(one instanceof CustomEntity);
+        assertFalse(one.isComplete());
+        d = (CustomEntity) one;
+        assertEquals(d.getQualifiedName(), child1.getQualifiedName());
+        assertEquals(d.getName(), child1.getName());
+        assertEquals(d.getConnectionQualifiedName(), connection.getQualifiedName());
+
+        one = entities.get(2);
+        assertTrue(one instanceof CustomEntity);
+        assertFalse(one.isComplete());
+        d = (CustomEntity) one;
+        assertEquals(d.getQualifiedName(), child2.getQualifiedName());
+        assertEquals(d.getName(), child2.getName());
         assertEquals(d.getConnectionQualifiedName(), connection.getQualifiedName());
     }
 
     @Test(
-            groups = {"custom.delete.entity"},
+            groups = {"custom.delete.child2"},
             dependsOnGroups = {"custom.update.*", "custom.search.*"})
-    void deleteEntity() throws AtlanException {
-        AssetMutationResponse response = Asset.delete(client, entity.getGuid()).block();
+    void deleteChild2() throws AtlanException {
+        AssetMutationResponse response = Asset.delete(client, child2.getGuid()).block();
         assertNotNull(response);
         assertTrue(response.getCreatedAssets().isEmpty());
         assertTrue(response.getUpdatedAssets().isEmpty());
@@ -136,36 +218,36 @@ public class CustomAssetTest extends AtlanLiveTest {
         Asset one = response.getDeletedAssets().get(0);
         assertTrue(one instanceof CustomEntity);
         CustomEntity s = (CustomEntity) one;
-        assertEquals(s.getGuid(), entity.getGuid());
-        assertEquals(s.getQualifiedName(), entity.getQualifiedName());
+        assertEquals(s.getGuid(), child2.getGuid());
+        assertEquals(s.getQualifiedName(), child2.getQualifiedName());
         assertEquals(s.getDeleteHandler(), "SOFT");
         assertEquals(s.getStatus(), AtlanStatus.DELETED);
     }
 
     @Test(
-            groups = {"custom.delete.entity.read"},
-            dependsOnGroups = {"custom.delete.entity"})
+            groups = {"custom.delete.child2.read"},
+            dependsOnGroups = {"custom.delete.child2"})
     void readDeletedEntity() throws AtlanException {
-        validateDeletedAsset(entity, log);
+        validateDeletedAsset(child2, log);
     }
 
     @Test(
-            groups = {"custom.delete.entity.restore"},
-            dependsOnGroups = {"custom.delete.entity.read"})
+            groups = {"custom.delete.child2.restore"},
+            dependsOnGroups = {"custom.delete.child2.read"})
     void restoreEntity() throws AtlanException {
-        assertTrue(CustomEntity.restore(client, entity.getQualifiedName()));
-        CustomEntity restored = CustomEntity.get(client, entity.getQualifiedName());
+        assertTrue(CustomEntity.restore(client, child2.getQualifiedName()));
+        CustomEntity restored = CustomEntity.get(client, child2.getQualifiedName());
         assertFalse(restored.isComplete());
-        assertEquals(restored.getGuid(), entity.getGuid());
-        assertEquals(restored.getQualifiedName(), entity.getQualifiedName());
+        assertEquals(restored.getGuid(), child2.getGuid());
+        assertEquals(restored.getQualifiedName(), child2.getQualifiedName());
         assertEquals(restored.getStatus(), AtlanStatus.ACTIVE);
     }
 
     @Test(
-            groups = {"custom.purge.entity"},
-            dependsOnGroups = {"custom.delete.entity.restore"})
+            groups = {"custom.purge.child2"},
+            dependsOnGroups = {"custom.delete.child2.restore"})
     void purgeEntity() throws AtlanException {
-        AssetMutationResponse response = Asset.purge(client, entity.getGuid()).block();
+        AssetMutationResponse response = Asset.purge(client, child2.getGuid()).block();
         assertNotNull(response);
         assertTrue(response.getCreatedAssets().isEmpty());
         assertTrue(response.getUpdatedAssets().isEmpty());
@@ -173,8 +255,8 @@ public class CustomAssetTest extends AtlanLiveTest {
         Asset one = response.getDeletedAssets().get(0);
         assertTrue(one instanceof CustomEntity);
         CustomEntity s = (CustomEntity) one;
-        assertEquals(s.getGuid(), entity.getGuid());
-        assertEquals(s.getQualifiedName(), entity.getQualifiedName());
+        assertEquals(s.getGuid(), child2.getGuid());
+        assertEquals(s.getQualifiedName(), child2.getQualifiedName());
         assertEquals(s.getDeleteHandler(), "PURGE");
         assertEquals(s.getStatus(), AtlanStatus.DELETED);
     }
@@ -186,7 +268,7 @@ public class CustomAssetTest extends AtlanLiveTest {
                 "custom.read.*",
                 "custom.search.*",
                 "custom.update.*",
-                "custom.purge.entity"
+                "custom.purge.child2"
             },
             alwaysRun = true)
     void purgeConnection() throws AtlanException, InterruptedException {
