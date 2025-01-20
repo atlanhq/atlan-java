696442bd856e40aed3e2a11bafca332fd8cb3ec9
prateekrai-atlan <prateek.rai@atlan.com>
2025-01-20 13:57:41 +0530

    ApplicationField Creator + generator + App integration Test

--- sdk/src/main/java/com/atlan/model/assets/ApplicationField.java
+++ sdk/src/main/java/com/atlan/model/assets/ApplicationField.java
@@ -8,6 +8,7 @@ import com.atlan.exception.ErrorCode;
 import com.atlan.exception.InvalidRequestException;
 import com.atlan.exception.NotFoundException;
 import com.atlan.model.enums.AtlanAnnouncementType;
+import com.atlan.model.enums.AtlanConnectorType;
 import com.atlan.model.enums.CertificateStatus;
 import com.atlan.model.fields.AtlanField;
 import com.atlan.model.relations.Reference;
@@ -331,6 +332,68 @@ public class ApplicationField extends Asset implements IApplicationField, IApp, 
      */
     public static boolean restore(AtlanClient client, String qualifiedName) throws AtlanException {
         return Asset.restore(client, TYPE_NAME, qualifiedName);
+    }
+
+    /**
+     * Builds the minimal object necessary to create a ApplicationField.
+     *
+     * @param name of the field
+     * @param application in which the field should be created, which must have at least
+     *                 a qualifiedName
+     * @return the minimal request necessary to create the field, as a builder
+     * @throws InvalidRequestException if the application provided is without a qualifiedName
+     */
+    public static ApplicationField.ApplicationFieldBuilder<?, ?> creator(String name, Application application)
+        throws InvalidRequestException {
+        Map<String, String> map = new HashMap<>();
+        map.put("applicationQualifiedName", application.getQualifiedName());
+        map.put("connectionQualifiedName", application.getConnectionQualifiedName());
+        validateRelationship(Application.TYPE_NAME, map);
+        return creator(name, application.getConnectionQualifiedName(), application.getQualifiedName())
+            .application(application.trimToReference());
+    }
+
+    /**
+     * Builds the minimal object necessary to create a ApplicationField.
+     *
+     * @param name of the field
+     * @param applicationQualifiedName unique name of the application in which this field exists
+     * @return the minimal request necessary to create the field, as a builder
+     */
+    public static ApplicationField.ApplicationFieldBuilder<?, ?> creator(String name, String applicationQualifiedName) {
+        String connectionQualifiedName = StringUtils.getParentQualifiedNameFromQualifiedName(applicationQualifiedName);
+        return creator(name, connectionQualifiedName, applicationQualifiedName);
+    }
+
+    /**
+     * Builds the minimal object necessary to create a ApplicationField.
+     *
+     * @param name of the field
+     * @param connectionQualifiedName unique name of the connection in which to create the field
+     * @param applicationQualifiedName unique name of the application in which to create the field
+     * @return the minimal request necessary to create the field, as a builder
+     */
+    public static ApplicationField.ApplicationFieldBuilder<?, ?> creator(
+        String name, String connectionQualifiedName, String applicationQualifiedName) {
+        AtlanConnectorType connectorType = Connection.getConnectorTypeFromQualifiedName(connectionQualifiedName);
+        return ApplicationField._internal()
+            .guid("-" + ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE - 1))
+            .name(name)
+            .qualifiedName(generateQualifiedName(name, applicationQualifiedName))
+            .connectionQualifiedName(connectionQualifiedName)
+            .connectorType(connectorType)
+            .parentApplication(Application.refByQualifiedName(applicationQualifiedName));
+    }
+
+    /**
+     * Generate a unique field name.
+     *
+     * @param name of the field
+     * @param applicationQualifiedName unique name of the application in which this field exists
+     * @return a unique name for the field
+     */
+    public static String generateQualifiedName(String name, String applicationQualifiedName) {
+        return applicationQualifiedName + "/" + name;
     }
 
     /**
