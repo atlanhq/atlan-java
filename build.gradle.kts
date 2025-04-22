4a5fb80fe31a1407e6d1cbc634d439063a68885c
Chris (He/Him) <cgrote@gmail.com>
2025-04-22 17:51:59 +0100

    Dependency bumps
    Signed-off-by: Chris (He/Him) <cgrote@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.atlan.java")
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
-    id("org.owasp.dependencycheck") version "12.1.0"
+    id("org.owasp.dependencycheck") version "12.1.1"
 }
 
 group = providers.gradleProperty("GROUP").get()
