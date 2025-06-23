4650f2d86e6c9754510cd7d5ae594e038fa1305b
Chris (He/Him) <cgrote@gmail.com>
2025-06-23 09:51:52 +0100

    Remove earlier model iterations
    Signed-off-by: Chris (He/Him) <cgrote@gmail.com>

--- sdk/src/main/java/com/atlan/model/assets/ICatalog.java
+++ sdk/src/main/java/com/atlan/model/assets/ICatalog.java
@@ -341,35 +341,11 @@ public interface ICatalog {
             case DynamoDBTable.TYPE_NAME:
                 ref = DynamoDBTable.refByQualifiedName(qualifiedName);
                 break;
-            case ETLControlFlow.TYPE_NAME:
-                ref = ETLControlFlow.refByQualifiedName(qualifiedName);
-                break;
-            case ETLControlFlowOperation.TYPE_NAME:
-                ref = ETLControlFlowOperation.refByQualifiedName(qualifiedName);
-                break;
-            case ETLDataFlowOperation.TYPE_NAME:
-                ref = ETLDataFlowOperation.refByQualifiedName(qualifiedName);
-                break;
-            case ETLExecutableUnit.TYPE_NAME:
-                ref = ETLExecutableUnit.refByQualifiedName(qualifiedName);
-                break;
             case File.TYPE_NAME:
                 ref = File.refByQualifiedName(qualifiedName);
                 break;
             case FivetranConnector.TYPE_NAME:
                 ref = FivetranConnector.refByQualifiedName(qualifiedName);
-                break;
-            case FlowInterimDataset.TYPE_NAME:
-                ref = FlowInterimDataset.refByQualifiedName(qualifiedName);
-                break;
-            case FlowInterimField.TYPE_NAME:
-                ref = FlowInterimField.refByQualifiedName(qualifiedName);
-                break;
-            case FlowV02InterimDataset.TYPE_NAME:
-                ref = FlowV02InterimDataset.refByQualifiedName(qualifiedName);
-                break;
-            case FlowV02InterimField.TYPE_NAME:
-                ref = FlowV02InterimField.refByQualifiedName(qualifiedName);
                 break;
             case FlowV03InterimDataset.TYPE_NAME:
                 ref = FlowV03InterimDataset.refByQualifiedName(qualifiedName);
@@ -388,12 +364,6 @@ public interface ICatalog {
                 break;
             case Insight.TYPE_NAME:
                 ref = Insight.refByQualifiedName(qualifiedName);
-                break;
-            case InterimDataset.TYPE_NAME:
-                ref = InterimDataset.refByQualifiedName(qualifiedName);
-                break;
-            case InterimField.TYPE_NAME:
-                ref = InterimField.refByQualifiedName(qualifiedName);
                 break;
             case KafkaConsumerGroup.TYPE_NAME:
                 ref = KafkaConsumerGroup.refByQualifiedName(qualifiedName);
