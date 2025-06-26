e3bf010aabfbfd86a94f5fa7d2a0e555bccf3db4
Chris (He/Him) <cgrote@gmail.com>
2025-06-26 12:02:23 +0100

    Revised user-defined relationship handling to not be restricted to terms
    Signed-off-by: Chris (He/Him) <cgrote@gmail.com>

--- package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/serde/cell/AssetRefXformer.kt
+++ package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/serde/cell/AssetRefXformer.kt
@@ -108,27 +108,51 @@ object AssetRefXformer {
         assetRef: String,
         fieldName: String,
     ): Asset {
-        return when (fieldName) {
-            Asset.README.atlanFieldName -> Readme._internal().description(assetRef).build()
+        val (ref, properties) = AbstractRelationshipAttributesXformer.getRefAndProperties(assetRef)
+        // For any-type-to-any-type relationships, we need to apply additional
+        // logic to detect the type based on something other than fieldName
+        val fieldOverrideForType = when (fieldName) {
+            in UserDefRelationshipXformer.USER_DEF_RELN_FIELDS -> {
+                val typeName = ref.substringBefore(TYPE_QN_DELIMITER)
+                when (typeName) {
+                    "TERM" -> "assignedTerms"
+                    "CATEGORY" -> GlossaryTerm.CATEGORIES.atlanFieldName
+                    "GLOSSARY" -> GlossaryCategory.ANCHOR.atlanFieldName
+                    "DOMAIN" -> DataProduct.DATA_DOMAIN.atlanFieldName
+                    // TODO: Data products and model assets are currently unhandled
+                    else -> fieldName
+                }
+            }
+            else -> fieldName
+        }
+        val baseRef = when (fieldOverrideForType) {
+            Asset.README.atlanFieldName -> Readme._internal().description(ref).build()
             Asset.LINKS.atlanFieldName -> {
-                val (linkJson, semantic) = getSemantic(assetRef)
+                val (linkJson, semantic) = getSemantic(ref)
                 val link = ctx.client.readValue(linkJson, Link::class.java)
                 link.toBuilder().semantic(semantic).build()
             }
             GlossaryCategory.PARENT_CATEGORY.atlanFieldName,
             GlossaryTerm.CATEGORIES.atlanFieldName,
-            -> GlossaryCategoryXformer.decode(ctx, assetRef, fieldName)
-            GlossaryCategory.ANCHOR.atlanFieldName -> GlossaryXformer.decode(ctx, assetRef, fieldName)
-            "assignedTerms", in GlossaryTermXformer.TERM_TO_TERM_FIELDS -> GlossaryTermXformer.decode(ctx, assetRef, fieldName)
-            DataDomain.PARENT_DOMAIN.atlanFieldName, DataProduct.DATA_DOMAIN.atlanFieldName -> DataDomainXformer.decode(ctx, assetRef, fieldName)
-            in ModelAssetXformer.MODEL_ASSET_REF_FIELDS -> ModelAssetXformer.decode(ctx, assetRef, fieldName)
-            in UserDefRelationshipXformer.USER_DEF_RELN_FIELDS -> UserDefRelationshipXformer.decode(ctx, assetRef, fieldName)
+            -> GlossaryCategoryXformer.decode(ctx, ref, fieldOverrideForType)
+            GlossaryCategory.ANCHOR.atlanFieldName -> GlossaryXformer.decode(ctx, ref, fieldOverrideForType)
+            "assignedTerms", in GlossaryTermXformer.TERM_TO_TERM_FIELDS -> GlossaryTermXformer.decode(ctx, ref, fieldOverrideForType)
+            DataDomain.PARENT_DOMAIN.atlanFieldName, DataProduct.DATA_DOMAIN.atlanFieldName -> DataDomainXformer.decode(ctx, ref, fieldOverrideForType)
+            in ModelAssetXformer.MODEL_ASSET_REF_FIELDS -> ModelAssetXformer.decode(ctx, ref, fieldOverrideForType)
             else -> {
-                val (ref, semantic) = getSemantic(assetRef)
+                val (ref, semantic) = getSemantic(ref)
                 val typeName = ref.substringBefore(TYPE_QN_DELIMITER)
                 val qualifiedName = ref.substringAfter(TYPE_QN_DELIMITER)
-                return getRefByQN(ctx, typeName, qualifiedName, semantic)
+                getRefByQN(ctx, typeName, qualifiedName, semantic)
             }
+        }
+        // Extend the baseRef with additional details
+        return when (fieldName) {
+            in UserDefRelationshipXformer.USER_DEF_RELN_FIELDS -> {
+                val (_, semantic) = getSemantic(ref)
+                UserDefRelationshipXformer.decode(ctx, baseRef, semantic, properties)
+            }
+            else -> baseRef
         }
     }
 
