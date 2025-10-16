b514549048c79dfaf0b36a9253f50e71d0ed2f5c
Chris (He/Him) <cgrote@gmail.com>
2025-10-16 11:05:34 +0100

    Linting fixes
    Signed-off-by: Chris (He/Him) <cgrote@gmail.com>

--- sdk/src/main/java/com/atlan/api/PermissionsEndpoint.java
+++ sdk/src/main/java/com/atlan/api/PermissionsEndpoint.java
@@ -4,28 +4,9 @@ package com.atlan.api;
 
 import com.atlan.AtlanClient;
 import com.atlan.exception.AtlanException;
-import com.atlan.model.admin.ApiToken;
-import com.atlan.model.admin.ApiTokenResponse;
 import com.atlan.model.admin.PermissionsResponse;
 import com.atlan.net.ApiResource;
 import com.atlan.net.RequestOptions;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Set;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Singular;
 
 /**
  * API endpoints for retrieving evaluated permissions.
