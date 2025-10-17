00a94bc834850b9191fca6526e7a342a8d5ab954
Chris (He/Him) <cgrote@gmail.com>
2025-10-17 10:20:43 +0100

    Adds detection of trying to write too many rows to Excel
    Signed-off-by: Chris (He/Him) <cgrote@gmail.com>

--- package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/serde/TabularWriter.kt
+++ package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/serde/TabularWriter.kt
@@ -2,23 +2,35 @@
    Copyright 2023 Atlan Pte. Ltd. */
 package com.atlan.pkg.serde
 
+import java.io.Closeable
+import java.io.IOException
+
 /**
  * Generic interface through which to write out tabular content.
  */
-interface TabularWriter {
+abstract class TabularWriter : Closeable {
+    protected val header = mutableListOf<String>()
+
     /**
      * Create a header row for the tabular output.
      *
      * @param headers ordered map of header names and descriptions
      */
-    fun writeHeader(headers: Map<String, String>)
+    fun writeHeader(headers: Map<String, String>) {
+        if (headers.isNotEmpty()) {
+            writeHeader(headers.keys)
+        }
+    }
 
     /**
      * Create a header row for the tabular output.
      *
      * @param values ordered list of header column names
      */
-    fun writeHeader(values: Iterable<String>)
+    open fun writeHeader(values: Iterable<String>) {
+        header.addAll(values)
+        writeRecord(values)
+    }
 
     /**
      * Write a row of data into the tabular output, where key of the map is the column name and the value
@@ -26,13 +38,25 @@ interface TabularWriter {
      * Note: be sure you have first called {@code writeHeader} to output the header row.
      *
      * @param values map keyed by column name with values for the row of data
+     * @throws IOException if the data cannot be written
      */
-    fun writeRecord(values: Map<String, Any?>?)
+    @Throws(IOException::class)
+    fun writeRecord(values: Map<String, Any?>?) {
+        if (values != null) {
+            val list = mutableListOf<Any>()
+            header.forEach { name ->
+                list.add(values.getOrDefault(name, "") ?: "")
+            }
+            writeRecord(list)
+        }
+    }
 
     /**
      * Add a row of data to the end of the tabular output.
      *
      * @param data the row of data to add
+     * @throws IOException if the data cannot be written
      */
-    fun writeRecord(data: Iterable<Any?>?)
+    @Throws(IOException::class)
+    abstract fun writeRecord(data: Iterable<Any?>?)
 }
--- package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/serde/csv/CSVWriter.kt
+++ package-toolkit/runtime/src/main/kotlin/com/atlan/pkg/serde/csv/CSVWriter.kt
@@ -9,7 +9,6 @@ import de.siegmar.fastcsv.writer.CsvWriter
 import de.siegmar.fastcsv.writer.LineDelimiter
 import de.siegmar.fastcsv.writer.QuoteStrategies
 import mu.KLogger
-import java.io.Closeable
 import java.io.IOException
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicLong
@@ -26,8 +25,7 @@ class CSVWriter
     constructor(
         path: String,
         fieldSeparator: Char = ',',
-    ) : Closeable,
-        TabularWriter {
+    ) : TabularWriter() {
         private val writer =
             CsvWriter
                 .builder()
@@ -37,53 +35,14 @@ class CSVWriter
                 .lineDelimiter(LineDelimiter.PLATFORM)
                 .build(ThreadSafeWriter(path))
 
-        private val header = mutableListOf<String>()
-
-        /**
-         * Write a header row into the CSV file.
-         * Note: since this is a CSV output, the description will be dropped (no standard way to add
-         * a comment to a CSV file).
-         *
-         * @param headers ordered map of header names and descriptions
-         */
-        override fun writeHeader(headers: Map<String, String>) {
-            header.addAll(headers.keys)
-            writeRecord(headers.keys)
-        }
-
-        /**
-         * Write a header row into the CSV file.
-         *
-         * @param values to use for the header
-         */
-        override fun writeHeader(values: Iterable<String>) {
-            header.addAll(values)
-            writeRecord(values)
-        }
-
-        /**
-         * Write a row of data into the CSV file, where key of the map is the column name and the value
-         * is the value to write for that column of the row of data.
-         * Note: be sure you have first called {@code writeHeader} to output the header row.
-         *
-         * @param values map keyed by column name with values for the row of data
-         */
-        override fun writeRecord(values: Map<String, Any?>?) {
-            if (values != null) {
-                val list = mutableListOf<Any>()
-                header.forEach { name ->
-                    list.add(values.getOrDefault(name, "") ?: "")
-                }
-                writeRecord(list)
-            }
-        }
-
         /**
          * Write a row of data into the CSV file, where the values are already sequenced
          * in the same order as the header columns.
          *
          * @param data to use for the row of data
+         * @throws IOException if the data cannot be written
          */
+        @Throws(IOException::class)
         override fun writeRecord(data: Iterable<Any?>?) {
             if (data != null) {
                 synchronized(writer) { writer.writeRecord(data.map { it?.toString() ?: "" }) }
