0735ecfbc53ba645e49813d7be876ab1c48fa539
Chris (He/Him) <cgrote@gmail.com>
2025-11-25 17:52:06 +0000

    Bump dependencies
    Signed-off-by: Chris (He/Him) <cgrote@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,8 +12,8 @@ dependencies {
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.2.21")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.1.0")
     implementation("io.freefair.gradle:lombok-plugin:9.1.0")
-    implementation("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:4.2.0")
+    implementation("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:4.3.0")
     implementation("com.adarshr:gradle-test-logger-plugin:4.0.0")
     implementation("org.pkl-lang:org.pkl-lang.gradle.plugin:0.30.0")
-    implementation("org.wiremock:wiremock-jetty12:3.13.1")
+    implementation("org.wiremock:wiremock-jetty12:3.13.2")
 }
