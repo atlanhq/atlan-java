5b7795b10e599902c717965836db97c21667a112
Chris (He/Him) <cgrote@gmail.com>
2025-12-04 12:34:56 +0000

    Fixes passthrough of connection qualifiedName for asset import
    Signed-off-by: Chris (He/Him) <cgrote@gmail.com>

--- samples/packages/asset-import/src/main/kotlin/com/atlan/pkg/aim/ConnectionImporter.kt
+++ samples/packages/asset-import/src/main/kotlin/com/atlan/pkg/aim/ConnectionImporter.kt
@@ -104,10 +104,14 @@ class ConnectionImporter(
             val roles = deserializer.getValue(Connection.ADMIN_ROLES.atlanFieldName)?.let { it as List<String> }
             val ct = AtlanConnectorType.fromValue(resolvedType)
             if (ct != null && ct != AtlanConnectorType.UNKNOWN_CUSTOM) {
-                Connection.creator(ctx.client, resolvedName, ct, roles, groups, users)
+                Connection
+                    .creator(ctx.client, resolvedName, ct, roles, groups, users)
+                    .qualifiedName(qualifiedName) // If a qualifiedName was sent explicitly, keep it (i.e., for connection widget passthrough or where multiple connections of the same type and name exist)
             } else {
                 val category = deserializer.getValue(Connection.CATEGORY.atlanFieldName)?.let { it as AtlanConnectionCategory }
-                Connection.creator(ctx.client, resolvedName, resolvedType, category, roles, groups, users)
+                Connection
+                    .creator(ctx.client, resolvedName, resolvedType, category, roles, groups, users)
+                    .qualifiedName(qualifiedName) // If a qualifiedName was sent explicitly, keep it (i.e., for connection widget passthrough or where multiple connections of the same type and name exist)
             }
         }
     }
